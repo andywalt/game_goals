16b176c0cd1c0e99ce231a80cef4992ae39288c2
Sparklebeard <tom.the.cub@gmail.com>
2020-01-06 16:56:44 -0800

    Some examples with comments, hope it helps!

--- Game Detail/GameGoalListView.swift
+++ Game Detail/GameGoalListView.swift
@@ -13,28 +13,40 @@ import CoreData
 struct GameGoalListView: View {
     
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Game.entity(), sortDescriptors: []) var games: FetchedResults<Game>
+    // We are going to pass in the specific Goal we are working with below, so we aren't fetching any data from CoreData here
+    //    @FetchRequest(entity: Game.entity(), sortDescriptors: []) var games: FetchedResults<Game>
     
-    @ObservedObject var game: Game
+    // We want to work with Goals on this view, so we don't care about Games.
+    //    @ObservedObject var game: Game
+    @ObservedObject var goal: Goal
     
-    @State private var goalComplete : Bool = false
     
+    // Using @State creates a new source of truth, which we don't want to do. In this case you want to use the Bool already defined on the Goal object.
+    //    @State private var goalComplete : Bool = false
     
     var body: some View {
         VStack {
-            ForEach(game.goalArray, id: \.goalName) { goal in
-                HStack {
-                    Text(goal.goalName ?? "No Goal Name")
-                    Spacer()
-                    Text("Complete:").font(.caption)
-                    Image(systemName: self.goalComplete ? "checkmark.square.fill" : "app").onTapGesture {
-                        self.goalComplete.toggle()
-                        print(self.goalComplete)
-                    }
+            
+            // Remove the ForEach here. Since you're passing in the Goal you don't need to iterate through all the goals in the array. The ForEach on GameGoalsDetail will create a new GameGoalListView for each Goal in the goalArray. Because of this, your GameGoalListView only needs to know about one goal :)
+            //          ForEach(game.goalArray, id: \.goalName) { goal in
+            HStack {
+                Text(goal.goalName ?? "No Goal Name")
+                Spacer()
+                Text("Complete:").font(.caption)
+                // Use the goalComplete already defined on the goal object rather than makinga new boolean on the view.
+                Image(systemName: self.goal.goalComplete ? "checkmark.square.fill" : "app").onTapGesture {
+                    
+                    self.goal.goalComplete.toggle()
+                    print(self.goal.goalComplete)
                 }
+                
             }
+            
         }
-        
+            // Use the objectWillChange publisher in your NSManagedObject to tell you that one of its published properties (any with @NSManaged) changed, and use that trigger to tell the managed object context to perform a save operation
+            .onReceive(self.goal.objectWillChange) {
+                try? self.moc.save()
+        }
     }
 }
 
@@ -44,6 +56,6 @@ struct GameGoalListView_Previews: PreviewProvider {
         let newGame = Game(context: context)
         newGame.gameName = "Apex Legends"
         newGame.gameDescription = "Maybe this will work"
-        return GameGoalListView(game: newGame).environment(\.managedObjectContext, context)
+        return GameGoalListView(goal: Goal()).environment(\.managedObjectContext, context)
     }
 }
--- Game Detail/GameGoalsDetail.swift
+++ Game Detail/GameGoalsDetail.swift
@@ -7,15 +7,18 @@
 //
 
 import SwiftUI
-import CoreData
+//import CoreData
 
 struct GameGoalsDetail: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Game.entity(), sortDescriptors: []) var games: FetchedResults<Game>
     
+    // The Game is passed into the view below, so it's not neccesary to reference a fetch request on this view :) This is effectively just a fancy array of games, but we only care about the game given to us by the previous view
+    //    @FetchRequest(entity: Game.entity(), sortDescriptors: []) var games: FetchedResults<Game>
+    //
     @State private var showingAddGoal = false
     
-    @State private var goalComplete : Bool = false
+    // I don't think anything is using this property, and it would be creating a new, disconencted source of truth. Does it need to be here?
+    //    @State private var goalComplete : Bool = false
     
     @ObservedObject var game: Game
     
@@ -25,14 +28,20 @@ struct GameGoalsDetail: View {
             Text(self.game.gameDescription ?? "No Game Description").font(.subheadline)
             List {
                 ForEach(game.goalArray, id: \.self)  { goal in
-                    GameGoalListView(game: self.game).environment(\.managedObjectContext, self.moc)
+                    // You want to show a cell that knows about a Goal. It doesn't need to know about the game, its just showing one Goal. So you'd want to pass in the goal directly, which is why you have the "goal in" in the closure :)
+                    //                    GameGoalListView(game: self.game).environment(\.managedObjectContext, self.moc)
+                    
+                    // You don't need to pass in the managed object context if you're staying within the view hierarchy (in this case staying inside the NavigationView). This is because sub-views inherit the environment of their parent view. You do need to pass it to .sheet because sheets are outside the view hierarchy and so do not inherit the environment!
+                    //                    GameGoalListView(game: game)
+                    GameGoalListView(goal: goal)
+                    
                 }
             }
             Button("Add Game Goal") {
                 self.showingAddGoal.toggle()
             }
-                .sheet(isPresented: $showingAddGoal) {
-                    AddGameGoalsView(game: self.game).environment(\.managedObjectContext, self.moc)
+            .sheet(isPresented: $showingAddGoal) {
+                AddGameGoalsView(game: self.game).environment(\.managedObjectContext, self.moc)
             }
         }
     }
