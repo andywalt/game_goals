11e81e9a05bc3387b90fa1fd1db387f7f3b6cdc6
Sparklebeard <tom.the.cub@gmail.com>
2020-01-12 01:18:34 -0800

    View model approach is working. I Have not tested the onAppear approach given here but it should work. HJopefully this isn't too confusing, My initial advice wasn't quite correct, and i ended up putting together an example of a view model for this, since it works better than the onAppear approach.
    Because the view model is a bit of a major change in structure i also left in commented out code that you could enable to try the .onAppear approach. I find that there are UI glitches with the onAppear approach. For example, in a page with a segmented picker control, if you change the value onAppear you get a weird 'wobble' in the picker control when the view appears as the value is set. This doesn't happen with the view model approach.
    
    I also noted a couple of things that might help regarding layout stuff. Feel free to ignore if im misunderstanding your design goal!

--- Game Detail/EditGameView.swift
+++ Game Detail/EditGameView.swift
@@ -7,47 +7,104 @@
 //
 
 import SwiftUI
+import Combine
+
+/* This is a basic view model. It contains the state and logic for the view so that the view can be dumb. It also means we can get around annoying initialiser restrictions because of state */
+class EditViewModel: ObservableObject {
+    
+    @Published var newGameName: String
+    @Published var newGameDescription: String
+    @Published var game: Game
+    
+    init(game: Game) {
+        self.game = game
+        self.newGameDescription = game.gameDescription ?? ""
+        self.newGameName = game.gameName ?? ""
+        
+        // This is using combine to subscribe to "objectWillChange" subjects from the Game and forward them to the view as part of the ObservableObject magic. Dont get too worried about this.
+        // $game is a publisher that publishes type Passthroughsubject<Void, Never>, which is just a blank token that tells us that the observed object changed.
+        self.$game
+            // sink just means "use the values received through this subscription in this closure"
+            .sink { _ in
+                // we just use "_ in" rather than like "value in" or whatever because we dont care about the actual value here (and indeed, its just a blank item) but we DO care about it allowing us to trigger an action. In this case, we use it to call the send() method on EditViewModel's own objectWillChange property (its a property that is automatically inherited via the ObservableObject protocol, but you can declare it yourself at the top of the class scope with "let objectWillChange = Passthroughsubject<Void, Never>()" You dont need to though, and don't get confused by this. This is not super important right now! The important thing is to just understand that this objectWillChange publisher is the secret sauce that makes the view update when the @ObservedObject changes. The view automatically subscribes to this publisher and refreshes the view every time it receives objectWillChange from this object.
+                //  So to recap, the view model receives the "Hey i changed!" message from GAME, and then the view model sends a new "Hey i changed!" message to the view, which responds by rebuilding its body.
+                self.objectWillChange.send()
+        }
+            // I'm still trying to underand what store does (the combine book is so thick omg), but i do know that it doesn't work without this so.....  (It's basically some way to store the publisher and its values in a way that iOS can clean up the memory for cleanly.. or something, idk!
+        .store(in: &cancellables)
+    }
+    // Also confusing> This has something to do with subcribers (Cancellables). Necessary for store.
+    private var cancellables = Set<AnyCancellable>()
+}
 
 struct EditGameView: View {
     
     @Environment(\.managedObjectContext) var moc
     
-    // not sure I'll need this
-    //@ObservedObject var game: Game
+    /*
+   You need this if you don't use a view model (see below)
+     */
+//    @ObservedObject var game: Game // @State
     
-    @State var newGameName = ""
-    @State var newGameDescription = ""
+    /* .onAppear has some potential side effects (UI 'glitches' as things update as the view loads), but you can avoid this by using a view model. This is more complex than i originally intended but i was wrong when i thought you could use init() in the struct here. I was conufsed. */
+    @ObservedObject var model: EditViewModel // View Model
+    
+//    @State var newGameName: String = "" // @State
+//    @State var newGameDescription: String // @State
 
+    /*
+     I originally thought you could use init() here but i forgot that you can't initialise a @State variable.
+     If you dont like the view model and want to see the .onAppear method, uncomment the @State  and then comment out all the ViewModel stuff
+     */
     
     var body: some View {
         List {
-            VStack {
-                Text("Game Details").bold()
+            VStack { // This VStack is crushing this into one cell. Are you perhaps looking for Section?
+//            Section {
+                Text("Game Details").bold() // You might like to put this in the navigation bar title (see below)
                 Divider()
-                TextField("Game Name:", text: $newGameName)
-                TextField("Game Description:", text: $newGameDescription)
+                TextField("Game Name:", text: $model.newGameName) // View Model
+                TextField("Game Description:", text: $model.newGameDescription) // View Model
+//                TextField("Game Name:", text: $newGameName) // @State
+//                TextField("Game Description:", text: $newGameDescription).onAppear { // @State
+//                    self.newGameName = self.game.gameName ?? "" // @State
+//                    self.newGameDescription = self.game.gameDescription ?? "" // @State
+//            } // @State
             }
-            HStack {
+//            HStack { // Not needed unless you want to stack another view horizontally within the cell)
                 Button("Update Game") {
-                    self.init(game: Game) {
-                    self.newGameName = game.gameName
-                    self.newGameDescription = game.gameDescription
-                        
+                    
+                    /*
+                     • Presumably this button's task is to take the new values entered into the view, and use themn to replace the old values in the Game object
+                     • Flip these expressions above around so that you're assigning from the view to the game object:
+                     */
+
+                /*  existing object   <--      new values  */
+                    self.model.game.gameName = self.model.newGameName // View Model
+                    self.model.game.gameDescription = self.model.newGameDescription // View Model
+//                    self.game.gameName = self.newGameName // @State
+//                    self.game.gameDescription = self.newGameDescription // @State
+
+                        /* Even though the Game has new values, its only stored in RAM until we tell CoreData to change the value in the persistent store by saving */
                         do {
                             try self.moc.save()
                         } catch {
                             print(error.localizedDescription)
                         }
-                    }
-                }
+//                    }
+//                }
             }
         }
+//            //Navigation bar modifiers are weird. You need to put them INSIDE the navigation view. IT doesn't actually matter where inside this view you put it, since this view is wholly within the navigation view
+//            .navigationBarTitle(Text("Game Details"), displayMode: .inline)
+//        You can use a different title modifier within the details view (the part that gets switched out for this edit view) and the title would change as the edit mode changed.
+//        Uncomment if you'd like to experiment with this
     }
 }
 
 struct EditGameView_Previews: PreviewProvider {
     static var previews: some View {
-        EditGameView()
+        EditGameView(model: EditViewModel(game: Game()))
     }
 }
 
--- Game Detail/GameGoalsDetail.swift
+++ Game Detail/GameGoalsDetail.swift
@@ -11,29 +11,83 @@ import SwiftUI
 
 struct GameGoalsDetail: View {
     @Environment(\.managedObjectContext) var moc
-    @Environment(\.editMode) var mode
+//    @Environment(\.editMode) var mode // You can handle this as a view modifier on your List below :)
     
     @State private var showingAddGoal = false
         
     @ObservedObject var game: Game
+    @State var showingEdit: Bool = false // Only needed if not using the NavigationLink approach
     
     var body: some View {
         VStack {
-            HStack {
-                EditButton()
-            }
-            if self.mode?.wrappedValue == .inactive {
+//            HStack {
+                // I think this is for doing deletes or dragging to reorder in a list. I think this is causing problems with trying to get the edits to save.
+//                EditButton()
+                // Try just creating a simple bool.
+                Button(action: {
+                    self.showingEdit.toggle()
+                }) {
+                    Text("Edit")
+                }
+//            }
+            if !self.showingEdit {
                 Text(self.game.gameName ?? "No Game Name").font(.title)
                 Text(self.game.gameDescription ?? "No Game Description").font(.subheadline)
+                // Uncomment to experiment with switvhout out the navbar title when you change to edit mode
+//                    .navigationBarTitle(Text(self.game.gameName ?? "No Game Name"), displayMode: .inline)
             } else {
-                // I think this is how it needs to be called but not sure if I need
-                EditGameView(game: Game)
-                    .onAppear {
-                        // I think this is where the onAppear goes but I'd imagine that I have to create a whole method like self.updatedGameInfo.gameName = self.game.gameName & a  self.updatedGameInfo = self.game.gameDescription
+                /* Almost! You are using Game (type) rather than game (instance of Game). If you change that to game you will be able to pass the specific type of Game. You would only use the Type inside of brackets like this when you're defining a function, and not when you're calling a function. When calling the function you provide the value/instance of the Type since the function already knows what Type to expect :)
+                 
+                 If using the View Model approach, i think the easiest way is to create the view model here and then pass along to the view right here too.
+                 
+                 So here we initialise an EditGameView, which requires a model parameter, so we in-line also initliase EditViewModel which takes a Game. You could write this out
+                 
+                 let model: EditViewModel = EditViewModel(game: self.game)
+                 EditGameView(model: model)
+                 
+                 If you really wanted to.
+                 */
+                EditGameView(model: EditViewModel(game: game)) // View Model
+//                EditGameView(game: game) // @State
+                
+                
+//                    .onAppear {
+//                        // I think this is where the onAppear goes but I'd imagine that I have to create a whole method like self.updatedGameInfo.gameName = self.game.gameName & a  self.updatedGameInfo = self.game.gameDescription
+//                    }
+//                    .onDisappear {
+//                        // self.game.gameName = self.updatedGameInfo.gameName
+//                    }
+                
+                /* onAppear and onDisappear would need to be used within EditGameView itself. I'm actually not sure what they would do here. They cause the view in which they exist to perform some action when the view that they are modifying appears or disappears.
+                 
+                 Put differently,imagine you have something like this where you want an alert to pop up when the label "Goose" scrolls onto the screen
+                 
+                 struct DuckDuckGoose: View {
+                    var body: some View {
+                        ScrollView {
+                 
+                            Text("Duck")
+                            Text("Duck")
+                            Text("Duck")
+                            Text("Duck")
+                            Text("Duck")
+                            Text("Duck")
+                            Text("Duck")
+                            Text("Duck")
+                            Text("Goose!")
+                                .onAppear {
+                                    showAlertFunction()
+                                }
+                 
+                        }.font(.largeTitle)
+                 
                     }
-                    .onDisappear {
-                        // self.game.gameName = self.updatedGameInfo.gameName
-                    }
+                 }
+                 
+                 
+                 In this case, when Goose shows on screen in the view, the .onAppear modifier on the Text() item will trigger its closure. Likewise .onDisappear would trigger a closure when scrolling off the screen.
+
+                */
             }
             List {
                 ForEach(game.goalArray, id: \.self) { goal in
@@ -50,6 +104,8 @@ struct GameGoalsDetail: View {
                     }
                 }
             }
+                // You can control the editMode for the list here based on your showingEdit bool
+            .environment(\.editMode, .constant(self.showingEdit ? EditMode.active : EditMode.inactive))
             Button(action: {
                 self.showingAddGoal.toggle()
             }) {
